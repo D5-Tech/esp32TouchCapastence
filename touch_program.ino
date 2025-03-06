73ee7f6641a1a35d7c96737915778f304424fcb0
Devanarayanan <131556363+D5-Tech@users.noreply.github.com>
2025-03-06 22:41:54 +0530

    Update touch_program.ino
    led added

--- touch_program.ino
+++ touch_program.ino
@@ -1,9 +1,20 @@
+#define TOUCH_PIN T0  // Touch sensor pin (GPIO4)
+#define LED_PIN 2      // Built-in LED on GPIO2
+
 void setup() {
+  pinMode(LED_PIN, OUTPUT);  
   Serial.begin(115200);
 }
 
 void loop() {
-  int touchValue = touchRead(T0); // Read touch pin T0 (GPIO4)
-  Serial.println(touchValue);
+  int touchValue = touchRead(TOUCH_PIN);  // Read touch input
+  Serial.println(touchValue);  // Print value for debugging
+
+  if (touchValue < 30) {  // Adjust this threshold if needed
+    digitalWrite(LED_PIN, HIGH); // Turn LED ON
+  } else {
+    digitalWrite(LED_PIN, LOW);  // Turn LED OFF
+  }
+
   delay(100);
 }
